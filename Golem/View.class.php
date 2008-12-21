aeb5b381e176c6f835753b729e0c19f84513206c
Soft <soft@menestystarina.org>
2008-12-21 18:23:14 +0200

    View class added

--- Golem/View.class.php
+++ Golem/View.class.php
@@ -2,6 +2,28 @@
 
 class View {
 	
+	private $values = array();
+	
+	public function __construct($controller, $action) {
+		if (file_exists($this->getViewPath($controller, $action))) {
+			$this->Path = $this->getViewPath($controller, $action);
+		} else {
+			throw new Exception("Cannot find view for $action in controller $controller.");
+		}
+	}
+	
+	public function Set($key, $value) {
+		$this->values[$key] = $value;
+	}
+	
+	public function Render() {
+		var_export($this->values);
+		require($this->Path);
+	}
+	
+	private function getViewPath($controller, $action) {
+		return APPDIR . DS . "Views" . DS . $controller . DS . $action . ".view";
+	}
 	
 }
 

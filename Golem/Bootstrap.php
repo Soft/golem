fe78c467790c03c5b851d2602a9bd53eb896f4b5
Soft <soft@menestystarina.org>
2008-12-22 22:05:24 +0200

    Added check for PHP version

--- Golem/Bootstrap.php
+++ Golem/Bootstrap.php
@@ -1,6 +1,10 @@
 <?php
 
 error_reporting(E_ALL | E_STRICT);
+if (!version_compare(PHP_VERSION, "5.1.0", ">")) {
+	trigger_error("At least PHP 5.1.0 is required to run Golem. Installed PHP version is " . PHP_VERSION);
+	exit();
+}
 
 require_once("Router.class.php");
 
